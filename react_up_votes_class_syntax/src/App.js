1b5ce7a5b111d9cb44546e100236ca6e9e949e64
nevenjo7 <neven.josipovic.sae@gmail.com>
2022-04-01 10:38:05 +0200

    react voting project, written with function and class components

---
+++ react_up_votes_class_syntax/src/App.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+
+class ProductList extends React.Component {
+  state = {
+    products: [],
+  };
+
+  componentDidMount() {
+    this.setState({ products: Seed.products });
+  }
+
+  handleProductUpVote = (productId) => {
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({
+      products: nextProducts,
+    });
+  }
+
+  render() {
+    const products = this.state.products.sort((a, b) => (
+      b.votes - a.votes
+    ));
+    const productComponents = products.map((product) => (
+      <Product
+        key={'product-' + product.id}
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+        onVote={this.handleProductUpVote}
+      />
+    ));
+    return (
+      <div className='ui unstackable items'>
+        {productComponents}
+      </div>
+    );
+  }
+}
+
+class Product extends React.Component {
+  handleUpVote = () => (
+    this.props.onVote(this.props.id)
+  );
+
+  render() {
+    return (
+      <div className='item' style={{margin: "20px"}}>
+        <div className='image'>
+          <img src={this.props.productImageUrl} alt="Product"/>
+        </div>
+        <div className='middle aligned content'>
+          <div className='header'>
+            <a onClick={this.handleUpVote} href="#">
+              <i className='large caret up icon' />
+            </a>
+            {this.props.votes}
+          </div>
+          <div className='description'>
+            <a href={this.props.url}>
+              {this.props.title}
+            </a>
+            <p>
+              {this.props.description}
+            </p>
+          </div>
+          <div className='extra'>
+            <span>Submitted by:</span>
+            <img
+              className='ui avatar image'
+              src={this.props.submitterAvatarUrl}
+              alt="Avatar"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+// SEED
+const Seed = (function () {
+  function generateVoteCount() {
+    return Math.floor((Math.random() * 50) + 15);
+  }
+
+  const products = [
+    {
+      id: 1,
+      title: 'Yellow Pail',
+      description: 'On-demand sand castle construction expertise.',
+      url: '#',
+      votes: generateVoteCount(),
+      submitterAvatarUrl: '/images/avatars/daniel.jpg',
+      productImageUrl: '/images/products/image-aqua.png',
+    },
+    {
+      id: 2,
+      title: 'Supermajority: The Fantasy Congress League',
+      description: 'Earn points when your favorite politicians pass legislation.',
+      url: '#',
+      votes: generateVoteCount(),
+      submitterAvatarUrl: '/images/avatars/kristy.png',
+      productImageUrl: '/images/products/image-rose.png',
+    },
+    {
+      id: 3,
+      title: 'Tinfoild: Tailored tinfoil hats',
+      description: 'We already have your measurements and shipping address.',
+      url: '#',
+      votes: generateVoteCount(),
+      submitterAvatarUrl: '/images/avatars/veronika.jpg',
+      productImageUrl: '/images/products/image-steel.png',
+    },
+    {
+      id: 4,
+      title: 'Haught or Naught',
+      description: 'High-minded or absent-minded? You decide.',
+      url: '#',
+      votes: generateVoteCount(),
+      submitterAvatarUrl: '/images/avatars/molly.png',
+      productImageUrl: '/images/products/image-yellow.png',
+    },
+  ];
+
+  return { products: products };
+}());
+
+export default ProductList;
